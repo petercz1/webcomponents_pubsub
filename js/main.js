33f547e4b6dbe41187ab39bf32890a662f9afb2d
petercz1 <email.peter.carroll@gmail.com>
2019-04-09 16:49:19 +0800

    You dialed 5483.

--- js/main.js
+++ js/main.js
@@ -5,13 +5,3 @@ import './components/app-console.js';
 import './components/app-addperson.js';
 import './components/app-chosenpeople.js';
 import './components/app-chosenperson.js';
-
-class demo{
-	readout(){
-		console.log('reading...');
-	}
-}
-
-function dim(){
-	console.log('dimo');
-}
