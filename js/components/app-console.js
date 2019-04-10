dce8bcf4ef34f51e3f4db7254a21efcfd65f0ccc
petercz1 <email.peter.carroll@gmail.com>
2019-04-10 14:57:02 +0800

    All the troubles you have will pass away very quickly.

--- js/components/app-console.js
+++ js/components/app-console.js
@@ -6,7 +6,7 @@ class appConsole extends HTMLElement {
 		this.pubsub = PubSub;
 		this.renderData = this.renderData.bind(this);
 		this.renderData(this.pubsub.getData('getPeople', null));
-		this.pubsub.subscribe('newPerson', 'getPeople', null, this.renderData);
+		this.pubsub.subscribe('NewPerson', 'getPeople', null, this.renderData);
   }
 
 	// renders all data from 'getPeople()'
