c7eee2e98e8c96fc771ee62ee7b5e28212fa7014
petercz1 <email.peter.carroll@gmail.com>
2019-04-10 14:40:09 +0800

    Truth is the most valuable thing we have -- so let us economize it. 		-- Mark Twain

--- js/pubsub/pubsub.js
+++ js/pubsub/pubsub.js
@@ -29,6 +29,7 @@ class PubSub {
     });
 
     // alert all susbscribers to new/changed data
+    console.log(this.subscribers);
     this.subscribers.filter(subscriber => (subscriber.newInfo == newInfo)).forEach((subscriber) => {
       subscriber.callback(this.datastore.getRequest(subscriber.request));
     });
