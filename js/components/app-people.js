3ea6d7669982e247d0ffdb9f3ba06e134438efe3
petercz1 <email.peter.carroll@gmail.com>
2019-04-10 15:12:28 +0800

    You will soon meet a person who will play an important role in your life.

--- js/components/app-people.js
+++ js/components/app-people.js
@@ -8,7 +8,7 @@ class appPeople extends HTMLElement {
     this.renderData = this.renderData.bind(this);
     this.renderData(this.pubsub.getData('getPeople',null));
     this.pubsub.subscribe('NewPerson', 'getPeople', null, this.renderData);
-    this.pubsub.subscribe('NewPerson', 'getPeople', null, this.renderData);
+    this.pubsub.subscribe('DeletePerson', 'getPeople', null, this.renderData);
   }
 
   // renders all people returned from 'getPeople()'
