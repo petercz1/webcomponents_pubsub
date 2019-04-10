5ba186b919f28a1313507c597b00c9bd3d2bb65f
petercz1 <email.peter.carroll@gmail.com>
2019-04-10 15:12:08 +0800

    Today's weirdness is tomorrow's reason why. 		-- Hunter S. Thompson

--- js/components/app-people.js
+++ js/components/app-people.js
@@ -8,6 +8,7 @@ class appPeople extends HTMLElement {
     this.renderData = this.renderData.bind(this);
     this.renderData(this.pubsub.getData('getPeople',null));
     this.pubsub.subscribe('NewPerson', 'getPeople', null, this.renderData);
+    this.pubsub.subscribe('NewPerson', 'getPeople', null, this.renderData);
   }
 
   // renders all people returned from 'getPeople()'
