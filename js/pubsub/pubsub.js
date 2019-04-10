4e7c0fecf6a7bdf329b1a1e179aa33ffd62a81e3
petercz1 <email.peter.carroll@gmail.com>
2019-04-10 15:08:01 +0800

    Be security conscious -- National defense is at stake.

--- js/pubsub/pubsub.js
+++ js/pubsub/pubsub.js
@@ -28,7 +28,6 @@ class PubSub {
     });
 
     // alert all susbscribers to new/changed data
-    console.log(this.subscribers);
     this.subscribers.filter(subscriber => (subscriber.newInfo == newInfo)).forEach((subscriber) => {
       subscriber.callback(this.datastore.getRequest(subscriber));
     });
