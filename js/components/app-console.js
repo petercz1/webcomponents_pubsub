8dd5576d2571930126456f9ca4780ad0caf0b51c
petercz1 <email.peter.carroll@gmail.com>
2019-04-10 15:14:18 +0800

    Questionable day.
    Ask somebody something.

--- js/components/app-console.js
+++ js/components/app-console.js
@@ -8,7 +8,7 @@ class appConsole extends HTMLElement {
 		this.renderData(this.pubsub.getData('getPeople', null));
 		this.pubsub.subscribe('NewPerson', 'getPeople', null, this.renderData);
 		this.pubsub.subscribe('ChangePerson', 'getPeople', null, this.renderData);
-		this.pubsub.subscribe('ChangePerson', 'getPeople', null, this.renderData);
+		this.pubsub.subscribe('DeletePerson', 'getPeople', null, this.renderData);
   }
 
 	// renders all data from 'getPeople()'
