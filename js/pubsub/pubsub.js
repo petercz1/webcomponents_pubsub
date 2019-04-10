1f2d7bc779a802a2f8c57dfa3747b536552ea8fa
petercz1 <email.peter.carroll@gmail.com>
2019-04-10 15:06:58 +0800

    The only way to keep your health is to eat what you don't want, drink what you don't like, and do what you'd rather not. 		-- Mark Twain

--- js/pubsub/pubsub.js
+++ js/pubsub/pubsub.js
@@ -30,7 +30,6 @@ class PubSub {
     // alert all susbscribers to new/changed data
     console.log(this.subscribers);
     this.subscribers.filter(subscriber => (subscriber.newInfo == newInfo)).forEach((subscriber) => {
-      console.log(subscriber);
       subscriber.callback(this.datastore.getRequest(subscriber));
     });
   }
